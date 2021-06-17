15a2e55611b96e1f143669b77f98ec8f33420a27
Alex Bauruel <abauruel@gmail.com>
2021-06-17 09:01:35 -0300

    add service

---
+++ src/Avanade.SubTCSE.Projeto.Api/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
@@ -0,0 +1,12 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services
+{
+    public class EmployeeRoleService : IEmployeeRoleService
+    {
+        void IEmployeeRoleService.AddEmployeeRole(Entities.EmployeeRole employeeRole)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
