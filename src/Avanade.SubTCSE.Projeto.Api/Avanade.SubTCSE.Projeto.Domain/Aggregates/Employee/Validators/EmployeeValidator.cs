f69b39357b1879b59daa183e8aacfc0a9a6a2793
Alex Bauruel <abauruel@gmail.com>
2021-06-23 09:13:41 -0300

    feat: add new entity and your services and validators related

---
+++ src/Avanade.SubTCSE.Projeto.Api/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Validators/EmployeeValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Validators
+{
+    public class EmployeeValidator : AbstractValidator<Employee.Entities.Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleSet("new", () =>
+            {
+                RuleFor(e => e.FirstName).NotEmpty().WithMessage("{PropertyName} cannot be empty");
+            });
+        }
+    }
+}
